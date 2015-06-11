215f533e78c9f968b91ecd52dcec1ea34a89e5d7
Ahmad N. Raja <ar@penneo.com>
2015-06-11 19:02:24 +0200

    Added the authentication functionality
    A sample file for showing how to talk to the Penneo REST API.
    
    At the moment some code has been copied from an opensource nodejs wsse
    library. Need to refactor so that we use npm for the dependency.

---
+++ sample.js
@@ -0,0 +1,6 @@
+var config = require('./config.json');
+var Penneo = require('./Penneo.js');
+
+var penneo = new Penneo();
+penneo.init(config.key, config.secret, config.endpoint);
+penneo.request('GET', 'casefile/message/templates/default');
