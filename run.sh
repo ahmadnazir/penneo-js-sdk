6804d0c434848baf87e377f072978cb6bed3f25c
Ahmad N. Raja <ar@penneo.com>
2015-10-02 17:17:35 +0200

    Updated the package.json
    Also,
    - changed the user for the docker container
    - added a new request

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it -v `pwd`:/app google/nodejs  /bin/bash
+docker run -it -v `pwd`:/app -u `id -u $USER` google/nodejs  /bin/bash
--- sample.js
+++ sample.js
@@ -3,4 +3,5 @@ var Penneo = require('./Penneo.js');
 
 var penneo = new Penneo();
 penneo.init(config.key, config.secret, config.endpoint);
-penneo.request('GET', 'casefile/message/templates/default');
+// penneo.request('GET', 'casefile/message/templates/default');
+penneo.request('GET', 'casefiles');
